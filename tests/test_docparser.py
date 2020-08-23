a022084bd60da601464a39e8f8506369ca910b54
Ryan <ryanrigato@gmail.com>
2020-08-23 08:18:19 -0500

    s3upload task definition

--- tests/test_docparser.py
+++ tests/test_docparser.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from unittest.mock import MagicMock
+from unittest.mock import mock_open
 from unittest.mock import patch
 
 import json
@@ -13,6 +14,9 @@ class UnitTests(unittest.TestCase):
     def setUpClass(cls):
         cls.markdown_files_list = ["cloudformation-execute-changeset.md", "welcome.md", "task-reference.md", "ecr-pushimage.md", "security.md", "setting-up.md", "disaster-recovery-resiliency.md", "index.md", "send-message.md", "security-iam.md", "getting-started.md", "elastic-beanstalk-createversion.md", "awspowershell-module-script.md", "cloudformation-create-update.md", "secretsmanager-create-update.md", "tutorial-s3.md", "lambda-netcore-deploy.md", "systemsmanager-setparameter.md", "document-history.md", "codedeploy-deployment.md", "data-protection.md", "secretsmanager-getsecret.md", "compliance-validation.md", "elastic-beanstalk-deploy.md", "awsshell.md", "systemsmanager-runcommand.md", "lambda-deploy.md", "tutorial-eb.md", "s3-download.md", "s3-upload.md", "aws-cli.md", "lambda-invoke.md", "cloudformation-delete-stack.md", "systemsmanager-getparameter.md", "tutorials.md", "infrastructure-security.md"]
         cls.devops_task_list = ["Common", "CloudFormationExecuteChangeSet", "SystemsManagerRunCommand", "AWSCLI", "BeanstalkCreateApplicationVersion", "AWSPowerShellModuleScript", "SecretsManagerGetSecret", "ECRPushImage", "S3Download", "SystemsManagerSetParameter", "S3Upload", "CodeDeployDeployApplication", "CloudFormationCreateOrUpdateStack", "ECRPullImage", "BeanstalkDeployApplication", "SystemsManagerGetParameter", "AWSShellScript", "CloudFormationDeleteStack", "LambdaDeployFunction", "LambdaInvokeFunction", "SecretsManagerCreateOrUpdateSecret", "SendMessage", "LambdaNETCoreDeploy"]
+
+        with open("tests/tasks/s3upload.json") as s3_upload_task:
+            cls.mock_s3_task_definition = json.load(s3_upload_task)
 
     @patch("builtins.open")
     @patch("os.listdir")
@@ -56,22 +60,26 @@ class UnitTests(unittest.TestCase):
         )
 
 
-
+    @patch("docparser.docparser.get_matching_doc")
     @patch("builtins.open")
     @patch("os.listdir")
     @patch("docparser.docparser.get_logger")
-    def test_main(self, getLogger_mock, listdir_mock, open_mock):
+    def test_main(self, get_logger_mock, listdir_mock, open_mock,
+        get_matching_doc_mock):
         """Test for main function
 
             Parameters
             ----------
-            getLogger_mock : unittest.mock.MagicMock
+            get_logger_mock : unittest.mock.MagicMock
                 Mock object used to patch get_logger for lambda handler
 
             listdir_mock : unittest.mock.MagicMock
                 Mock object to get list of markdown name
 
             open_mock : unittest.mock.MagicMock
+                Mock object for open reading of markdown file
+
+            get_matching_dock_mock : unittest.mock.MagicMock
                 Mock object for open reading of markdown file
 
             Returns
@@ -83,10 +91,13 @@ class UnitTests(unittest.TestCase):
         from docparser.docparser import main
         mock_source_code_location = "../mock_source"
         mock_docs_location = "../mock_docs"
-        main(aws_toolkit_source=mock_source_code_location, aws_toolkit_docs=mock_docs_location)
-        get_logger_mock.assertCalledOnceWith()
 
 
         listdir_mock.return_value = self.devops_task_list
+
+        open_mock.return_value = self.mock_s3_task_definition
+
+        main(aws_toolkit_source=mock_source_code_location, aws_toolkit_docs=mock_docs_location)
+        get_logger_mock.assert_called_once_with()
 
         self.assertEqual(open_mock.call_count, len(self.devops_task_list))
